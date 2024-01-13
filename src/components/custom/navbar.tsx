e4e8b619cd30e1c094fd4d786b2451739cea14a1
orbiter <lunareclipsewasshort@gmail.com>
2024-01-13 21:51:02 +0300

    trolling reverted

--- src/components/custom/navbar.tsx
+++ src/components/custom/navbar.tsx
@@ -107,7 +107,7 @@ export default function Navbar() {
 							<FaTelegram size="2em" />
 						</Link>
 						<Link
-							href="https://gitbook.cryptobots.me/cryptobots/how-to-start-playing-cryptobots-game"
+							href="https://ropasci.gitbook.io/"
 							className={`
 									h-full w-1/4
 									border-y-2 border-black border-r
